275076dbb8f889ed357b1f9a3452a9ac76670522
alirexaf <mr.afxali@gmail.com>
2023-04-14 19:07:22 +0330

    Initial commit

---
+++ Domain/Entities/Webinar.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public sealed class Webinar : Entity
+    {
+        public Webinar(Guid id, string name, DateTime scheduledOn)
+            :base(id)
+        {
+            Name = name;
+            ScheduledOn = scheduledOn;
+
+        }
+        public string Name { get; private set; }
+        public DateTime ScheduledOn { get; private set; }
+    }
+}
---
+++ Domain/Exceptions/WebinarNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Exceptions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public sealed class WebinarNotFoundException:NotFoundException
+    {
+        public WebinarNotFoundException(Guid webinarId)
+            : base($"The webinar with identifier {webinarId} was not found!")
+        {
+            
+        }
+    }
+}
